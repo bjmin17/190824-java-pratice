25085572523de1170b0e54acf2adb05469633ea7
bjmin17 <bjmin17@naver.com>
2019-08-24 16:07:53 +0900

    190824_java_practice

---
+++ src/chapter2/IntergerVariable.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class IntergerVariable {
+
+	public static void main(String[] args) {
+		short sVal = 10;
+		byte bVal = 20;
+		System.out.println(sVal + bVal);
+		
+		long num = 12345678900L;
+		
+		
+	}
+
+}
---
+++ src/hello/HelloJava.java
@@ -0,0 +1,16 @@
+/*
+ * Date : 2019. 08. 24
+ * Author : 
+ * Description : 자바 연습 프로그램입니다.
+ * Version : 1.0
+ */
+
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+	}
+
+}
